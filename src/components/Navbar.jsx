a05a1d9b622afa69c9a8d5cc4c50d3876140211d
KAVYAJAIN410 <kavyajain@KAVYAs-MacBook-Air.local>
2024-09-06 15:33:54 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { styles } from '../styles';
 import { navLinks } from '../constants/index';
 import { logo, menu, close } from '../assets';
