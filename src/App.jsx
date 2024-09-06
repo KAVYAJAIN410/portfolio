94b59cec0afef11cedb21d9f12e98c1a12cca6e8
KAVYAJAIN410 <kavyajain@KAVYAs-MacBook-Air.local>
2024-09-06 15:26:44 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import  {BrowserRouter} from 'react-router-dom'
+
 import {About,Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas} from './components';
 function App() {
   return (
-    <BrowserRouter>
    <div className='relative z-0 bg-primary'>
    <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
    <Navbar/>
@@ -19,7 +18,6 @@ function App() {
 
    </div> 
    </div>
-   </BrowserRouter>
   )
 }
 
