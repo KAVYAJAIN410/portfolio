2ac3d42c96940005de2ad5aad76aee4fb99b8116
KAVYAJAIN410 <kavyajain@KAVYAs-MacBook-Air.local>
2024-09-06 15:58:17 +0530

    done

--- src/assets/index.js
+++ src/assets/index.js
@@ -26,8 +26,8 @@ import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
+import carrent from "./carrent.mov";
+import jobit from "./jobit.mov";
 import tripguide from "./tripguide.png";
 
 export {
